c7bc77f3f2832f75dc009555fc9fa250c1f90cc9
Alex Ysupov <alexysupov@yandex.ru>
2020-10-06 17:29:21 +0700

    текст пушей на главной

---
+++ resources/lang/en/home.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'pushes' => 'Push notifications are always visible and hard to miss: subscribers will immediately see your message as soon as they visit the site. Simple and clear statistics: analyze and improve click-throughs, openings, and new subscriptions. High probability of subscribing to notifications: users immediately see a subscription request as soon as they visit the site. Free access to the service: send push notifications for free to up to 1000 subscribers',
+];
