eb72209384201b3c4066d84fcc4c0b48c5fab01e
EginKaman <egin.kaman@gmail.com>
2020-10-06 11:07:46 +0300

    Исправление тарифов.

--- resources/views/tariffs/index.blade.php
+++ resources/views/tariffs/index.blade.php
@@ -44,7 +44,47 @@
                     </div>
 
                     <div class="tariff tariff-sec">
-                       <tariff-form
+                        <form action="#" class="tariff-form">
+                            <div class="tarim-top">
+                                <h3 class="tariff-title">PRO</h3>
+                                <strong class="tariff-subtitle number-followers">@lang('от 30 000 подписчиков')</strong>
+                                <div class="tariff-slider"></div>
+                                <div class="tariff-slider-val">
+                                    <!-- <div class="tariff-slider__line"></div> -->
+                                    <div class="tariff-slider__item active" data-price="3900">
+                                        <span class="tariff-slider__point fs"></span>
+                                        <span class="tariff-slider__text">30к</span>
+                                    </div>
+                                    <div class="tariff-slider__item" data-price="6000">
+                                        <span class="tariff-slider__point sec"></span>
+                                        <span class="tariff-slider__text">60к</span>
+                                    </div>
+                                    <div class="tariff-slider__item" data-price="10000">
+                                        <span class="tariff-slider__point th"></span>
+                                        <span class="tariff-slider__text">200к</span>
+                                    </div>
+                                    <div class="tariff-slider__item" data-price="13400">
+                                        <span class="tariff-slider__point fr"></span>
+                                        <span class="tariff-slider__text">∞</span>
+                                    </div>
+                                </div>
+                                <ul class="tariff-offer">
+                                    <li>@lang('Отсутствие ссылки «Работает с помощью Push.Gun» в окне запроса подписки')</li>
+                                    <li>@lang('Приоритетная поддержка')</li>
+                                </ul>
+                                <label for="sale" class="tariff-check">
+                                    <input type="checkbox" name="sale" id="sale">
+                                    <span class="check"></span>
+                                    <span class="tariff-check__text">@lang('Подписка на год') - 20%</span>
+                                </label>
+                            </div>
+
+                            <div class="tariff-bottom">
+                                <span class="tariff-price"><span
+                                        id="tariff-price"> 3900</span> @lang('руб./мес.') </span>
+                                <input type="hidden" name="" class="followsCount" value="30к">
+
+                                <button-payment
                            public_id="{{ config('services.cloud_payments.public_id') }}"
                            account_id="{{ auth()->id() }}">
                        </tariff-form>
