72794bb24c154fe9b258d6450c9b32cc54aa50d6
EginKaman <egin.kaman@gmail.com>
2020-10-08 13:01:52 +0300

    Запоминание выбора.

--- resources/views/pushes/index.blade.php
+++ resources/views/pushes/index.blade.php
@@ -10,20 +10,21 @@
                             <div class="filter__popup_block w-50">
                                 <label class="filter__popup_label" for="firstDate-input">@lang('Начальная дата')</label>
                                 <input id="firstDate-input" class="datepicker-here filter__input" name="start"
-                                       type="text">
+                                       type="text" value="{{ request('start') }}">
                             </div>
                             <div class="filter__popup_block w-50">
                                 <label class="filter__popup_label" for="lastDate-input">@lang('Конечная дата')</label>
-                                <input id="lastDate-input" class="datepicker-here filter__input" name="end" type="text">
+                                <input id="lastDate-input" class="datepicker-here filter__input" name="end" type="text" value="{{ request('end') }}">
                             </div>
                             <div class="filter__popup_block">
                                 <label class="filter__popup_label" for="text-input">@lang('Текст')</label>
-                                <input id="text-input" class="filter__input" name="text" type="text">
+                                <input id="text-input" class="filter__input" name="text" type="text" value="{{ request('text') }}">
                             </div>
                             <div class="filter__popup_block">
                                 <label class="filter__popup_label" for="site">@lang('Сайт')</label>
                                 <select id="site" class="filter__input filter__selector" name="site" type="text">
                                     <option v-for="site in $store.state.sites.sites" :key="site.id"
+                                            :selected="site.id === {{ request('site') }}"
                                             :value="site.id">@{{ site.link }}
                                     </option>
                                 </select>
