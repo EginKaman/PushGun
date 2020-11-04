bb7d307110c40d19d884dea740c87c2821f533e7
abdulhamid-webdev <abdulhamid_bd@mail.ru>
2020-11-04 16:44:01 +0600

    Added style for Reseat Password pages

--- resources/js/components/UI/Modals/Login.vue
+++ resources/js/components/UI/Modals/Login.vue
@@ -1,17 +1,40 @@
 <template>
     <div class="blocker current">
         <div id="enter" class="modal" style="display: inline-block;">
-            <h2>{{ $t('Вход') }}</h2>
+            <h2>{{ $t("Вход") }}</h2>
             <form :action="action" method="POST" @submit.prevent="submit">
                 <div class="modal__inner">
-                    <input type="email" v-model="email" name="email" placeholder="E-mail" required>
-                    <input type="password" v-model="password" name="password" :placeholder="$t('Пароль')" required>
-                    <button type="submit" class="header-landing__wrapper__links_reg">
-                        {{ $t('Войти') }}
-                    </button>
+                    <input
+                        type="email"
+                        v-model="email"
+                        name="email"
+                        placeholder="E-mail"
+                        required
+                    />
+                    <input
+                        type="password"
+                        v-model="password"
+                        name="password"
+                        :placeholder="$t('Пароль')"
+                        required
+                    />
+                    <div class="row">
+                        <button
+                            type="submit"
+                            class="header-landing__wrapper__links_reg"
+                        >
+                            {{ $t("Войти") }}
+                        </button>
+                        <a href="password/reset">Забыли пароль?</a>
+                    </div>
                 </div>
             </form>
-            <a href="#close-modal" @click.prevent="$emit('close')" class="close-modal">Close</a>
+            <a
+                href="#close-modal"
+                @click.prevent="$emit('close')"
+                class="close-modal"
+                >Close</a
+            >
         </div>
     </div>
 </template>
@@ -22,37 +45,58 @@ export default {
     props: {
         action: {
             type: String,
-            default: '/login'
-        },
+            default: "/login"
+        }
     },
     data() {
         return {
-            email: '',
-            password: ''
-        }
+            email: "",
+            password: ""
+        };
     },
     methods: {
         submit() {
-            axios.post(this.action, {
-                email: this.email,
-                password: this.password
-            }).then(response => {
-                location.reload();
-            }).catch(error => {
-                let errors = '';
-                if (error.response.status === 403) {
-                    errors = error.response.statusText;
-                } else {
-                    errors = Object.values(error.response.data.errors);
-                    errors = errors.flat().join("\r\n")
-                }
-                this.$swal({
-                    title: this.$i18n.t('Ошибка!'),
-                    text: errors,
-                    icon: "error",
+            axios
+                .post(this.action, {
+                    email: this.email,
+                    password: this.password
+                })
+                .then(response => {
+                    location.reload();
+                })
+                .catch(error => {
+                    let errors = "";
+                    if (error.response.status === 403) {
+                        errors = error.response.statusText;
+                    } else {
+                        errors = Object.values(error.response.data.errors);
+                        errors = errors.flat().join("\r\n");
+                    }
+                    this.$swal({
+                        title: this.$i18n.t("Ошибка!"),
+                        text: errors,
+                        icon: "error"
+                    });
                 });
-            });
-        }
+        },
+        forgetPassword() {
+        } 
     }
-}
+};
 </script>
+<style>
+.row {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr;
+}
+
+.row a {
+    display: flex;
+    cursor: pointer;
+    justify-content: flex-start;
+    text-decoration: none;
+    align-items: center;
+    color:#282828;
+}
+</style>
