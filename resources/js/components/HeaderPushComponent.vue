4fce50462b1c27b310f557bcb38112a66e8e9889
ixandr1a <parenzashigaet@gmail.com>
2020-12-10 15:46:26 +0300

    ixandr1a

--- resources/js/components/HeaderPushComponent.vue
+++ resources/js/components/HeaderPushComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="nav__menu">
         <ul class="nav__menu_inner">
-            <li class="nav__menu_items one" v-if="sites.length < 1">
+            <li class="nav__menu_items one" >
                 <a class="nav__menu_link" href="/account">{{ $t('Все рассылки') }}</a>
             </li>
-            <li class="nav__menu_items two" v-if="sites.length < 1">
+            <li class="nav__menu_items two" >
                 <a class="nav__menu_link" href="/account/autoMailing">{{ $t('Авторассылки') }}</a>
             </li>
         </ul>
