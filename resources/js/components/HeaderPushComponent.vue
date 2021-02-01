3d63998dbdc820498835844a1de596663d839b36
ixandr1a <parenzashigaet@gmail.com>
2021-02-01 16:25:56 +0300

    fix

--- resources/js/components/HeaderPushComponent.vue
+++ resources/js/components/HeaderPushComponent.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="nav__menu">
     <ul class="nav__menu_inner">
-      <li class="nav__menu_items one" v-if="sites.length < 1">
+      <li class="nav__menu_items one">
         <a class="nav__menu_link" href="/account">{{ $t("Все рассылки") }}</a>
       </li>
-      <li class="nav__menu_items two" v-if="sites.length < 1">
+      <li class="nav__menu_items two">
         <a class="nav__menu_link" href="/account/autoMailing">{{
           $t("Авторассылки")
         }}</a>
       </li>
-      <li class="nav__menu_items three" v-if="sites.length < 1">
+      <li class="nav__menu_items three">
         <a class="nav__menu_link" href="/account/referal">{{
           $t("Реферальная программа")
         }}</a>
@@ -19,21 +19,8 @@
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
-
 export default {
   name: "HeaderSitesComponent",
-  computed: {
-    ...mapState({
-      sites: (state) => state.sites,
-    }),
-    ...mapGetters("sites", {
-      sites: "getSites",
-    }),
-  },
-  mounted() {
-    this.$store.dispatch("sites/FETCH_SITES");
-  },
 };
 </script>
 
