6fb91a94466d8e5f923903ff94bcc4fa094b39b3
abdulhamid_webdev <abdulhamid_bd@mail.ru>
2021-02-08 20:55:27 +0600

    back

--- database/seeds/TariffsTableSeeder.php
+++ database/seeds/TariffsTableSeeder.php
@@ -18,26 +18,26 @@ class TariffsTableSeeder extends Seeder
         ]);
         Tariff::create([
             'name' => 'PRO 30K',
-            'price_per_month' => 5,
-            'price_per_year' => 5,
+            'price_per_month' => 3900,
+            'price_per_year' => 37440,
             'max_followers' => 30000
         ]);
         Tariff::create([
             'name' => 'PRO 60K',
-            'price_per_month' => 5,
-            'price_per_year' => 5,
+            'price_per_month' => 6000,
+            'price_per_year' => 57600,
             'max_followers' => 60000
         ]);
         Tariff::create([
             'name' => 'PRO 200K',
-            'price_per_month' => 10,
-            'price_per_year' => 10,
+            'price_per_month' => 10000,
+            'price_per_year' => 96000,
             'max_followers' => 200000
         ]);
         Tariff::create([
             'name' => 'PRO 200K',
-            'price_per_month' => 5,
-            'price_per_year' => 5,
+            'price_per_month' => 40000,
+            'price_per_year' => 384000,
             'max_followers' => 0
         ]);
     }
