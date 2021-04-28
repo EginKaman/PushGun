6277a07543284b5c93657b459ccf02139e8b6ed1
abdulhamid_webdev <abdulhamid_bd@mail.ru>
2021-04-28 22:48:19 +0600

    nova tariff email

--- app/Nova/TariffEmail.php
+++ app/Nova/TariffEmail.php
@@ -2,31 +2,29 @@
 
 namespace App\Nova;
 
+use Laravel\Nova\Fields\ID;
 use Illuminate\Http\Request;
-use Laravel\Nova\Fields\BelongsTo;
-use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Fields\Currency;
 use Laravel\Nova\Fields\HasMany;
-use Laravel\Nova\Fields\ID;
 use Laravel\Nova\Fields\Number;
-use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class Tariff extends Resource
+class TariffEmail extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = \App\Tariff::class;
+    public static $model = \App\TariffEmail::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'name';
+    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -34,28 +32,18 @@ class Tariff extends Resource
      * @var array
      */
     public static $search = [
-        'name',
+        'id',
+        'name'
     ];
-
     public static function label()
     {
-        return __('Тарифы');
-    }
-
-    /**
-     * Get the displayable singular label of the resource.
-     *
-     * @return string
-     */
-    public static function singularLabel()
-    {
-        return __('Тариф');
+        return __('Почтовые тарифы');
     }
 
     /**
      * Get the fields displayed by the resource.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function fields(Request $request)
@@ -67,7 +55,7 @@ class Tariff extends Resource
                 ->rules('required', 'max:255'),
             Currency::make('Цена за месяц', 'price_per_month')->currency('RUB')->sortable(),
             Currency::make('Цена за год', 'price_per_year')->currency('RUB')->sortable(),
-            Number::make('Макс. кол-во подписчиков', 'max_followers')
+            Number::make('Макс. кол-во контактов', 'max_contacts')
                 ->sortable(),
             HasMany::make('Пользователи', 'users', User::class)
         ];
@@ -76,7 +64,7 @@ class Tariff extends Resource
     /**
      * Get the cards available for the request.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function cards(Request $request)
@@ -87,7 +75,7 @@ class Tariff extends Resource
     /**
      * Get the filters available for the resource.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function filters(Request $request)
@@ -98,7 +86,7 @@ class Tariff extends Resource
     /**
      * Get the lenses available for the resource.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function lenses(Request $request)
@@ -109,7 +97,7 @@ class Tariff extends Resource
     /**
      * Get the actions available for the resource.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return array
      */
     public function actions(Request $request)
